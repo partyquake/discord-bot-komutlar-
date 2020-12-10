e368fd133e12f7ec8094f08883a6c04099442b3c
Utku <31706007+utkusay955@users.noreply.github.com>
2020-12-10 08:59:32 +0300

    banner.js
    discord.js - v11 to v12

--- banner.js
+++ banner.js
@@ -1,9 +1,10 @@
-// Embed'lı banner yapar.
-// akinturtul.com
-
 const Discord = require('discord.js');
+// s
+module.exports.run = async (client, message, args) => {
+ name: 'banner',
+    description: 'Yazdığınız yazıyı bannera çevirir.',
+    usage: 'banner <yazı>',
 
-exports.run = async (client, message, args) => {
   const yazi = args.slice(0).join('+'); 
 
   if(!yazi) return message.channel.send(`Lütfen yazı yazın!`)
@@ -11,22 +12,12 @@ exports.run = async (client, message, args) => {
   .replace(' ', '+')
 
   
-  const akinjs = new Discord.RichEmbed()
-  .setTitle("Banner!")
-  .setColor("#2ECC71")
-  .setImage(akinbanner)
-  .setFooter('Banner Oluşturuldu!')
-  message.channel.send(akinjs)
-}
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [],
-    permLevel: 0 // Sadece yönetici yetkisi olanların kullanması için 3 yapınız.
-}
+  const akinjs = new Discord.MessageEmbed()
+            .setTitle("Banner!")
+          .setColor("#2ECC71")
+        .setImage(akinbanner)
+      .setFooter('Banner Oluşturuldu!')
+     message.channel.send(akinjs)
+   }
 
-exports.help = {
-    name: 'banner',
-    description: 'Yazdığınız yazıyı bannera çevirir.',
-    usage: 'banner <yazı>'
 }
